45d6c4c421da370e1da60838b1f3859295d8bb3d
Arisha Salman Naqvi <arish123890@gmail.com>
2024-12-17 22:05:33 +0500

    Initial Commit

---
+++ assignment1.js
@@ -0,0 +1,14 @@
+//1st Alert Statement
+alert("Welcome to Our Website!");
+
+//2nd Alert Statement
+alert("Please enter a valid password.");
+
+//3rd Alert Statement
+alert("Welcome to JavaScipt Language...\nHappy Coding!");
+
+//4th Alert Statement
+alert("Welcome to JavaScript Land...");
+alert("Happy Coding and Enjoy your experience!");
+
+
